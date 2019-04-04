85b05d194b7d821eaa5f4d36f476b544e6b12839
taihelsel <tai.bayard@gmail.com>
2019-04-04 17:58:42 -0400

    adjusted style on invetory list item

--- components/InventoryListItem.js
+++ components/InventoryListItem.js
@@ -3,18 +3,18 @@ import { StyleSheet, View, Text, TouchableHighlight } from "react-native";
 export default class InventoryListItem extends React.Component {
     handleTouch = e => {
         this.props.handleTouch({ data: this.props.data });
-      }
+    }
     render() {
         const { data } = this.props;
         return (
             <TouchableHighlight onPress={this.handleTouch}>
                 <View style={styles.container}>
-                    <View style={{ height: 100, width: 100, backgroundColor: "orange", }}>
+                    <View style={{ height: 80, width: 80, backgroundColor: "orange", }}>
                         <Text>IMG HERE</Text>
                     </View>
-                    <View style={{ flexDirection: "row", flexWrap: "wrap", flex: 1, backgroundColor: "blue", paddingLeft: 15 }} >
+                    <View style={{flex: 1, paddingLeft: 15 }} >
                         <Text style={[styles.detailsText, { fontSize: 25, fontWeight: "bold" }]}>{data.title}</Text>
-                        <Text style={styles.detailsText}>{data.desc}</Text>
+                        <Text style={styles.detailsText}>{`$${data.price.min} - $${data.price.max}`}</Text>
                     </View>
                     <View style={{ backgroundColor: "green", width: 45 }}>
                         <Text>Quick Add BTN</Text>
@@ -33,6 +33,6 @@ const styles = StyleSheet.create({
         marginVertical: 2,
     },
     detailsText: {
-        color: "white"
+        color: "white",
     }
 });

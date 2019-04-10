d290eb5b626710a6932c301cc8814b216af4c3eb
taihelsel <tai.bayard@gmail.com>
2019-04-10 03:54:58 -0400

    added image rendering for icon view

--- components/InventoryIconItem.js
+++ components/InventoryIconItem.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import { StyleSheet, View, Text, TouchableHighlight } from "react-native";
+import { StyleSheet, View, Text, TouchableHighlight, Image } from "react-native";
 
 export default InventoryIconItem = ({ title, data, handleTouch }) => {
-  return (
+  const { img } = data;
+  return typeof img === "undefined" ? (
     <TouchableHighlight style={styles.container} underlayColor="rgba(212, 212, 212, 0.25)" onPress={handleTouch({ data })}>
-      <View style={styles.contentContainer}>
+      <View style={[styles.contentContainer]}>
         <Text style={{ color: "white", fontSize: 20, textAlign: "center" }}>{title}</Text>
       </View>
     </TouchableHighlight>
 
-  );
+  ) : (
+      <TouchableHighlight style={styles.container} underlayColor="rgba(212, 212, 212, 0.25)" onPress={handleTouch({ data })}>
+        <View style={styles.imgContentContainer}>
+          <Image style={{
+            flex: 1,
+            resizeMode: 'contain'
+          }} source={{ uri: img }} />
+        </View>
+      </TouchableHighlight>
+    );
 }
 const styles = StyleSheet.create({
   container: {
@@ -17,9 +27,17 @@ const styles = StyleSheet.create({
     height: 170,
   },
   contentContainer: {
-    flex: 1,
     backgroundColor: "orange",
+    flex: 1,
     margin: 7,
     justifyContent: "center"
+  },
+  imgContentContainer: {
+    flex: 1,
+    margin: 7,
+    justifyContent: "center",
+    borderWidth: 1,
+    borderStyle: "solid",
+    borderColor: "grey",
   }
 });

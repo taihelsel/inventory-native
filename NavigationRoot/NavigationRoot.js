066e96eec9afe799145936800d4ae6dd386b38b9
taihelsel <tai.bayard@gmail.com>
2019-04-12 03:33:46 -0400

    converted navigtion root to a switch

--- NavigationRoot/NavigationRoot.js
+++ NavigationRoot/NavigationRoot.js
@@ -1,26 +1,25 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
-/*Test component*/
-const Test = () => {
-    return (
-        <View>
-            <Text>
-                Hello World
-            </Text>
-        </View>
-    );
-}
 /*Nav Stacks*/
 import ClerkNavigation from "./ClerkNavigation";
 export default class NavigationRoot extends Component {
     state = {
-        isLoggedIn: true,
+        userType: "clerk",
     };
     render() {
-        return this.state.isLoggedIn ? (
-            <ClerkNavigation />
-        ) : (
-                <Test />
-            )
+        switch (this.state.userType) {
+            case "clerk": {
+                return <ClerkNavigation />
+            }
+            case "box-handler": {
+                return <Text>Box Handler Screen Here</Text>
+            }
+            case "admin": {
+                return <Text>Admin Screen Here</Text>
+            }
+            default: {
+                return <Text>Show sign in options here</Text>
+            }
+        }
     }
 }

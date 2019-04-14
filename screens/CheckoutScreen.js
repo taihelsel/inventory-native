5fad650ebf3d5e6a24766567bfc68aa9e9a1726f
taihelsel <tai.bayard@gmail.com>
2019-04-14 11:26:59 -0400

    converted CheckoutScreen to functional component

--- screens/CheckoutScreen.js
+++ screens/CheckoutScreen.js
@@ -5,73 +5,67 @@ import { connect } from "react-redux";
 import { updateCart, deleteCartItem } from "../actions/cartActions";
 /*Components*/
 import InventoryListItemSwipeout from "../components/InventoryListItemSwipeout";
-class CheckoutScreen extends React.Component {
-  updateCartTotal = (barcode, amntTxt) => {
-    const { maxPrice, minPrice, cartData, updateCart } = this.props;
-    // setting vars
-    let currentCartMax = maxPrice,
-      currentCartMin = minPrice,
-      clonedCartData = { ...cartData },
-      itemData = clonedCartData[barcode],
-      price = itemData.price,
-      currentAmnt = itemData.amnt,
-      newAmnt = parseInt(amntTxt),
-      diff = Math.abs(currentAmnt - newAmnt),
-      newCartMin = 0,
-      newCartMax = 0;
-    // setting new data
-    console.log("new", newAmnt, "old", currentAmnt);
-    if (currentAmnt === newAmnt) return false;
-    if (currentAmnt > newAmnt) {
-      //subtract from current Cart min/max
-      newCartMax = currentCartMax - (price.max * diff);
-      newCartMin = currentCartMin - (price.min * diff);
-    } else {
-      //add to current Cart min/max
-      newCartMax = currentCartMax + (price.max * diff);
-      newCartMin = currentCartMin + (price.min * diff);
-    }
-    // updating with new data
-    itemData.amnt = newAmnt;
-    updateCart({
-      minPrice: newCartMin,
-      maxPrice: newCartMax,
-      cartData: clonedCartData
-    });
+const updateCartTotal = (maxPrice, minPrice, cartData, updateCart) => (barcode, amntTxt) => {
+  // setting vars
+  let currentCartMax = maxPrice,
+    currentCartMin = minPrice,
+    clonedCartData = { ...cartData },
+    itemData = clonedCartData[barcode],
+    price = itemData.price,
+    currentAmnt = itemData.amnt,
+    newAmnt = parseInt(amntTxt),
+    diff = Math.abs(currentAmnt - newAmnt),
+    newCartMin = 0,
+    newCartMax = 0;
+  // setting new data
+  if (currentAmnt === newAmnt) return false;
+  if (currentAmnt > newAmnt) {
+    //subtract from current Cart min/max
+    newCartMax = currentCartMax - (price.max * diff);
+    newCartMin = currentCartMin - (price.min * diff);
+  } else {
+    //add to current Cart min/max
+    newCartMax = currentCartMax + (price.max * diff);
+    newCartMin = currentCartMin + (price.min * diff);
   }
-  handleItemTouch = () => {
-    console.log("cart item touched");
-  }
-  handleDeleteTouch = key => {
-    const { deleteCartItem } = this.props;
-    deleteCartItem({ key });
-  }
-  buildSwipeoutBtns = k => (
-    [{
-      type: "delete",
-      text: "Delete",
-      onPress: () => this.handleDeleteTouch(k),
-      color: "white",
-      backgroundColor: "red",
-    }]
-  );
-  render() {
-    const { minPrice, maxPrice, cartData, deleteCartItem } = this.props;
-    return (Object.keys(cartData).length > 0) ? (
-      <View style={styles.container}>
-        <ScrollView contentContainerStyle={styles.contentContainer}>
-          <InventoryListItemSwipeout isCartView={true} updateCartTotal={this.updateCartTotal} buildSwipeoutBtns={this.buildSwipeoutBtns} data={cartData} handleTouch={handleItemTouch} />
-        </ScrollView>
-        <Text style={{ textAlign: "center", marginBottom: 15, fontSize: 20, fontWeight: "600" }}>
-          Price range: <Text style={styles.price}>{`$${minPrice} - $${maxPrice}`}</Text>
-        </Text>
+  // updating with new data
+  itemData.amnt = newAmnt;
+  updateCart({
+    minPrice: newCartMin,
+    maxPrice: newCartMax,
+    cartData: clonedCartData
+  });
+}
+const handleItemTouch = () => {
+  console.log("cart item touched");
+}
+const handleDeleteTouch = (key, deleteCartItem) => {
+  deleteCartItem({ key });
+}
+const buildSwipeoutBtns = deleteCartItem => k => (
+  [{
+    type: "delete",
+    text: "Delete",
+    onPress: () => handleDeleteTouch(k, deleteCartItem),
+    color: "white",
+    backgroundColor: "red",
+  }]
+);
+const CheckoutScreen = ({ minPrice, maxPrice, cartData, updateCart, deleteCartItem }) => {
+  return (Object.keys(cartData).length > 0) ? (
+    <View style={styles.container}>
+      <ScrollView contentContainerStyle={styles.contentContainer}>
+        <InventoryListItemSwipeout isCartView={true} updateCartTotal={updateCartTotal(maxPrice, minPrice, cartData, updateCart)} buildSwipeoutBtns={buildSwipeoutBtns(deleteCartItem)} data={cartData} handleTouch={handleItemTouch} />
+      </ScrollView>
+      <Text style={{ textAlign: "center", marginBottom: 15, fontSize: 20, fontWeight: "600" }}>
+        Price range: <Text style={styles.price}>{`$${minPrice} - $${maxPrice}`}</Text>
+      </Text>
+    </View>
+  ) : (
+      <View style={styles.container} >
+        <Text style={{ textAlign: "center", marginTop: 25, fontSize: 25, color: "black" }}>No items in cart</Text>
       </View>
-    ) : (
-        <View style={styles.container} >
-          <Text style={{ textAlign: "center", marginTop: 25, fontSize: 25, color: "black" }}>No items in cart</Text>
-        </View>
-      );
-  }
+    );
 }
 const styles = StyleSheet.create({
   container: {

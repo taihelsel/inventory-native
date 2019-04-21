e01a920bbc5a1b3fdd765763c5a899beb9571d5b
taihelsel <tai.bayard@gmail.com>
2019-04-21 16:17:21 -0400

    updated create user screen style

--- screens/CreateUserScreen.js
+++ screens/CreateUserScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet, TextInput, Switch } from "react-native";
+import { View, Text, StyleSheet, TextInput, Switch, TouchableOpacity } from "react-native";
 /*Components*/
 import BackButton from "../components/BackButton";
 
@@ -19,11 +19,16 @@ class CreateUserScreen extends Component {
         const { isAdmin } = this.state;
         this.setState({ isAdmin: !isAdmin });
     }
+    handleSubmitPress = e => {
+        const { navigation } = this.props;
+        console.log("send invite link");
+        navigation.goBack(null);
+    }
     render() {
         return (
             <View style={styles.container}>
                 <View style={styles.inputWrapper}>
-                    <Text style={styles.inputLabel}>Email</Text>
+                    <Text style={styles.inputLabel}>Email:</Text>
                     <TextInput
                         style={styles.input}
                         value={this.state.email}
@@ -37,6 +42,12 @@ class CreateUserScreen extends Component {
                         onValueChange={this.handleAdminCheck}
                     />
                 </View>
+                <View style={styles.submitContainer}>
+                    <TouchableOpacity style={styles.submit} underlayColor="transparent" onPress={this.handleSubmitPress}>
+                        <Text style={styles.submitText}>Send Invite Link</Text>
+                    </TouchableOpacity>
+                </View>
+
             </View>
         );
     }
@@ -44,16 +55,23 @@ class CreateUserScreen extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        justifyContent: "center"
     },
     inputWrapper: {
+        flexDirection: "row",
+        alignItems: "center",
         marginHorizontal: 50,
-        marginVertical: 20,
+        paddingBottom: 20,
     },
     inputLabel: {
-        textAlign: "center",
+        flex: 1,
         fontSize: 23,
+        textAlign: "center",
+        paddingVertical: 8,
+        marginRight: 5
     },
     input: {
+        flex: 3,
         paddingHorizontal: 8,
         paddingVertical: 8,
         backgroundColor: "white",
@@ -65,7 +83,10 @@ const styles = StyleSheet.create({
     },
     checkboxWrapper: {
         flexDirection: "row",
-        justifyContent: "center"
+        justifyContent: "flex-start",
+        alignItems: "center",
+        marginHorizontal: 50,
+        paddingBottom: 20,
     },
     checkbox: {
         width: 50,
@@ -73,6 +94,22 @@ const styles = StyleSheet.create({
     checkboxText: {
         fontSize: 23,
         marginRight: 5
+    },
+    submitContainer: {
+        height: 50,
+        marginHorizontal: 25,
+        marginTop: 50,
+    },
+    submit: {
+        flex: 1,
+        justifyContent: "center",
+        backgroundColor: "green",
+        borderRadius: 3,
+    },
+    submitText: {
+        fontSize: 20,
+        color: "white",
+        textAlign: "center",
     }
 });
 
